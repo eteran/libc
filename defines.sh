a673d5c93ff3cf938ce0e088c4123442043bb189
Evan Teran <evan.teran@gmail.com>
2016-12-21 02:18:46 -0500

    updating this script

--- defines.sh
+++ defines.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 FLAGS="-x c -march=native -std=c11 -pedantic -Wextra -Wall -ffreestanding -O2 -g3 -Wno-long-long"
-gcc-4.9.3 -m64 -dM -E $FLAGS - <<<'' | sort | uniq
+gcc -m64 -dM -E $FLAGS - <<<'' | sort | uniq
 
