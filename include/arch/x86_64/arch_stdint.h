a05ce94e16e87804ca29ed0f5460d82ea24cf0b7
Evan Teran <evan.teran@gmail.com>
2019-08-07 16:37:48 -0400

    fixing stdint.h slightly

--- include/arch/x86_64/arch_stdint.h
+++ include/arch/x86_64/arch_stdint.h
@@ -40,54 +40,66 @@ typedef uint64_t           uintmax_t;
 
 #if !defined(__cplusplus) || defined(__ISOCXX11_SOURCE) || defined(__STDC_LIMIT_MACROS)
 
-#define INT8_MAX   127
-#define INT16_MAX  32767
-#define INT32_MAX  2147483647
-#define INT64_MAX  9223372036854775807
-
+/* Signed integers : minimum value */
 #define INT8_MIN   (-127)
 #define INT16_MIN  (-32767)
 #define INT32_MIN  (-2147483647)
 #define INT64_MIN  (-9223372036854775807LL)
-
-#define UINT8_MAX   0xff
-#define UINT16_MAX  0xffff
-#define UINT32_MAX  0xffffffff
-#define UINT64_MAX  0xffffffffffffffffULL
-
-#define INTMAX_MIN      INT64_MIN
-#define INTMAX_MAX      INT64_MAX
-#define UINTMAX_MAX     UINT64_MAX
-
-#define INT_LEAST8_MIN  INT8_MIN
-#define INT_LEAST16_MIN INT16_MIN
-#define INT_LEAST32_MIN INT32_MIN
-#define INT_LEAST64_MIN INT64_MIN
-
-#define INT_LEAST8_MAX  INT8_MAX
-#define INT_LEAST16_MAX INT16_MAX
-#define INT_LEAST32_MAX INT32_MAX
-#define INT_LEAST64_MAX INT64_MAX
 
 #define INT_FAST8_MIN   INT8_MIN
 #define INT_FAST16_MIN  INT16_MIN
 #define INT_FAST32_MIN  INT32_MIN
 #define INT_FAST64_MIN  INT64_MIN
 
+#define INT_LEAST8_MIN  INT8_MIN
+#define INT_LEAST16_MIN INT16_MIN
+#define INT_LEAST32_MIN INT32_MIN
+#define INT_LEAST64_MIN INT64_MIN
+
+#define INTPTR_MIN      INT64_MIN
+#define INTMAX_MIN      INT64_MIN
+
+/* Signed integers : maximum value */
+#define INT8_MAX   127
+#define INT16_MAX  32767
+#define INT32_MAX  2147483647
+#define INT64_MAX  9223372036854775807
+
 #define INT_FAST8_MAX   INT8_MAX
 #define INT_FAST16_MAX  INT16_MAX
 #define INT_FAST32_MAX  INT32_MAX
 #define INT_FAST64_MAX  INT64_MAX
+
+#define INT_LEAST8_MAX  INT8_MAX
+#define INT_LEAST16_MAX INT16_MAX
+#define INT_LEAST32_MAX INT32_MAX
+#define INT_LEAST64_MAX INT64_MAX
+
+#define INTPTR_MAX      INT64_MAX
+#define INTMAX_MAX      INT64_MAX
+
+/* Unsigned integers : maximum value*/
+
+#define UINT8_MAX   0xff
+#define UINT16_MAX  0xffff
+#define UINT32_MAX  0xffffffff
+#define UINT64_MAX  0xffffffffffffffffULL
 
 #define UINT_FAST8_MAX  UINT8_MAX
 #define UINT_FAST16_MAX UINT16_MAX
 #define UINT_FAST32_MAX UINT32_MAX
 #define UINT_FAST64_MAX UINT64_MAX
 
-#define INTPTR_MIN      INT64_MIN
-#define INTPTR_MAX      INT64_MAX
-#define UINTPTR_MAX     UINT64_MAX
+#define UINT_LEAST8_MAX  UINT8_MAX
+#define UINT_LEAST16_MAX UINT16_MAX
+#define UINT_LEAST32_MAX UINT32_MAX
+#define UINT_LEAST64_MAX UINT64_MAX
 
+#define UINTPTR_MAX     UINT64_MAX
+#define UINTMAX_MAX     UINT64_MAX
+
+
+/* Other */
 #define PTRDIFF_MIN     INT64_MIN
 #define PTRDIFF_MAX     INT64_MAX
 
