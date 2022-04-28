4082ff5b81d8ebc3425cecc83120ae7053b25c0a
Evan Teran <evan.teran@gmail.com>
2022-04-28 10:50:48 -0400

    a few more tests

--- tests/stddef-test.c
+++ tests/stddef-test.c
@@ -1,7 +1,31 @@
+/* 100% Coverage */
 #undef NDEBUG
 #define _ELIBC_SOURCE
+#include "test_util.h"
 #include <assert.h>
+#include <stddef.h>
+
+struct S {
+	char m0;
+	double m1;
+	short m2;
+	char m3;
+};
+
+static void test_offsetof(void) {
+	assert(offsetof(struct S, m0) == 0);
+	assert(offsetof(struct S, m1) == 8);
+	assert(offsetof(struct S, m2) == 16);
+	assert(offsetof(struct S, m3) == 18);
+}
 
 int main(void) {
+	TYPE_DEFNIED(ptrdiff_t)
+	TYPE_DEFNIED(size_t)
+	TYPE_DEFNIED(wchar_t)
+	TYPE_DEFNIED(max_align_t)
+	_Static_assert(IS_SAME_TYPE(NULL, void *), "");
+
+	test_offsetof();
 	return 0;
 }
