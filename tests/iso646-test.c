79afdfd5d057439e605c88a5b0a128051aa86e7e
Evan Teran <evan.teran@gmail.com>
2022-05-17 13:44:52 -0400

    more test cleanup

--- tests/iso646-test.c
+++ tests/iso646-test.c
@@ -1,7 +1,42 @@
 #undef NDEBUG
 #define _ELIBC_SOURCE
 #include <assert.h>
+#include <iso646.h>
 
 int main(void) {
+
+	_Static_assert((1 and 1), "");
+	_Static_assert(not (1 and 0), "");
+	_Static_assert(not (1 and 0), "");
+	_Static_assert(not (0 and 0), "");
+
+	_Static_assert((1 or 0), "");
+	_Static_assert((0 or 1), "");
+	_Static_assert((1 or 1), "");
+	_Static_assert(not (0 or 0), "");
+
+	_Static_assert(not (1 xor 1), "");
+	_Static_assert((1 xor 0), "");
+	_Static_assert((1 xor 0), "");
+	_Static_assert(not (0 xor 0), "");
+
+	_Static_assert((compl 0x5a5a5a5au) == 0xa5a5a5a5u, "");
+
+	_Static_assert((1 bitand 1) == 1, "");
+	_Static_assert((1 bitand 0) == 0, "");
+	_Static_assert((1 bitand 0) == 0, "");
+	_Static_assert((0 bitand 0) == 0, "");
+
+	_Static_assert((1 bitor 0) == 1, "");
+	_Static_assert((0 bitor 1) == 1, "");
+	_Static_assert((1 bitor 1) == 1, "");
+	_Static_assert((0 bitor 0) == 0, "");
+
+/*
+#define and_eq &=
+#define not_eq !=
+#define or_eq |=
+#define xor_eq ^=
+*/
 	return 0;
 }
--- tests/stdnoreturn-test.c
+++ tests/stdnoreturn-test.c
@@ -1,6 +1,7 @@
 #undef NDEBUG
 #define _ELIBC_SOURCE
 #include <assert.h>
+#include <stdnoreturn.h>
 
 int main(void) {
 	return 0;
