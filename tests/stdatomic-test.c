7f361b353dcc484f2f1089a01e18b8c5abcfcf11
Evan Teran <evan.teran@gmail.com>
2023-01-13 18:10:35 -0500

    cleaning up build a bit

--- tests/stdatomic-test.c
+++ tests/stdatomic-test.c
@@ -4,12 +4,13 @@
 #include <stdatomic.h>
 
 int main(void) {
-
+#ifdef _HAS_C11
 	struct atomic_flag f = ATOMIC_FLAG_INIT;
 	atomic_flag_clear(&f);
 	_Bool b1 = atomic_flag_test_and_set(&f);
 	assert(b1 == 0);
 	_Bool b2 = atomic_flag_test_and_set(&f);
 	assert(b2 == 1);
+#endif
 	return 0;
 }
