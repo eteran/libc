3ebcc3cb09193371c5e83a23b9650245a5e3fe18
Evan Teran <evan.teran@gmail.com>
2023-10-12 07:16:51 -0400

    cleaning up system call stuff

--- src/sys/syscall.hpp
+++ src/sys/syscall.hpp
@@ -3,88 +3,122 @@
 #define SYSCALL_20110313_H_
 
 namespace elibc {
-#ifdef __i386__
-	inline long syscall(unsigned long num) {
-		long ret;
-		__asm__ __volatile__("int $0x80" : "=a"(ret) : "a"(num) : "ecx");
-		return ret;
-	}
+#if defined(__x86_64__)
+inline long syscall(long num) {
+	long ret;
+	register long n __asm__("rax") = (num);
 
-	template <class T1>
-	long syscall(unsigned long num, T1 a1) {
-		long ret;
-		__asm__ __volatile__("int $0x80" : "=a"(ret) : "a"(num), "b"(a1) : "ecx");
-		return ret;
-	}
+	__asm__ volatile(
+		"syscall\n"
+		: "=a"(ret)
+		: "0"(n)
+		: "rcx", "r11", "memory", "cc");
+	return ret;
+}
 
-	template <class T1, class T2>
-	long syscall(unsigned long num, T1 a1, T2 a2) {
-		long ret;
-		__asm__ __volatile__("int $0x80" : "=a"(ret) : "a"(num), "b"(a1), "c"(a2));
-		return ret;
-	}
+template <class T1>
+long syscall(long num, T1 a1) {
+	long ret;
+	register long n __asm__("rax")  = (num);
+	register long r1 __asm__("rdi") = (long)(a1);
 
-	template <class T1, class T2, class T3>
-	long syscall(unsigned long num, T1 a1, T2 a2, T3 a3) {
-		long ret;
-		__asm__ __volatile__("int $0x80" : "=a"(ret) : "a"(num), "b"(a1), "c"(a2), "d"(a3));
-		return ret;
-	}
-#elif defined(__x86_64__)
-	inline long syscall(unsigned long num) {
-		long ret;
-		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(num) : "memory", "rcx", "r11");
-		return ret;
-	}
+	__asm__ volatile(
+		"syscall\n"
+		: "=a"(ret)
+		: "r"(r1),
+		  "0"(n)
+		: "rcx", "r11", "memory", "cc");
+	return ret;
+}
 
-	template <class T1>
-	long syscall(unsigned long num, T1 a1) {
-		long ret;
-		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(num), "D"(a1) : "memory", "rcx", "r11");
-		return ret;
-	}
+template <class T1, class T2>
+long syscall(long num, T1 a1, T2 a2) {
+	long ret;
+	register long n __asm__("rax")  = (num);
+	register long r1 __asm__("rdi") = (long)(a1);
+	register long r2 __asm__("rsi") = (long)(a2);
 
-	template <class T1, class T2>
-	long syscall(unsigned long num, T1 a1, T2 a2) {
-		long ret;
-		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(num), "D"(a1), "S"(a2) : "memory", "rcx", "r11");
-		return ret;
-	}
+	__asm__ volatile(
+		"syscall\n"
+		: "=a"(ret)
+		: "r"(r1), "r"(r2),
+		  "0"(n)
+		: "rcx", "r11", "memory", "cc");
+	return ret;
+}
 
-	template <class T1, class T2, class T3>
-	long syscall(unsigned long num, T1 a1, T2 a2, T3 a3) {
-		long ret;
-		__asm__ __volatile__("syscall" : "=a"(ret) : "a"(num), "D"(a1), "S"(a2), "d"(a3) : "memory", "rcx", "r11");
-		return ret;
-	}
+template <class T1, class T2, class T3>
+long syscall(long num, T1 a1, T2 a2, T3 a3) {
+	long ret;
+	register long n __asm__("rax")  = (num);
+	register long r1 __asm__("rdi") = (long)(a1);
+	register long r2 __asm__("rsi") = (long)(a2);
+	register long r3 __asm__("rdx") = (long)(a3);
 
-	template <class T1, class T2, class T3, class T4>
-	long syscall(unsigned long num, T1 a1, T2 a2, T3 a3, T4 a4) {
-		long ret;
-		register long r10 __asm__("r10") = a4;
-		__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(num), "D"(a1), "S"(a2), "d"(a3), "r"(r10) : "memory", "rcx", "r11");
-		return ret;
-	}
+	__asm__ volatile(
+		"syscall\n"
+		: "=a"(ret)
+		: "r"(r1), "r"(r2), "r"(r3),
+		  "0"(n)
+		: "rcx", "r11", "memory", "cc");
+	return ret;
+}
 
+template <class T1, class T2, class T3, class T4>
+long syscall(long num, T1 a1, T2 a2, T3 a3, T4 a4) {
+	long ret;
+	register long n __asm__("rax")  = (num);
+	register long r1 __asm__("rdi") = (long)(a1);
+	register long r2 __asm__("rsi") = (long)(a2);
+	register long r3 __asm__("rdx") = (long)(a3);
+	register long r4 __asm__("r10") = (long)(a4);
 
-	template <class T1, class T2, class T3, class T4, class T5>
-	long syscall(unsigned long num, T1 a1, T2 a2, T3 a3, T4 a4, T5 a5) {
-		long ret;
-		register long r10 __asm__("r10") = a4;
-		register long r8 __asm__("r8") = a5;
-		__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(num), "D"(a1), "S"(a2), "d"(a3), "r"(r10), "r"(r8) : "memory", "rcx", "r11");
-		return ret;
-	}
+	__asm__ volatile(
+		"syscall\n"
+		: "=a"(ret)
+		: "r"(r1), "r"(r2), "r"(r3), "r"(r4),
+		  "0"(n)
+		: "rcx", "r11", "memory", "cc");
+	return ret;
+}
 
-	template <class T1, class T2, class T3, class T4, class T5, class T6>
-	long syscall(unsigned long num, T1 a1, T2 a2, T3 a3, T4 a4, T5 a5, T6 a6) {
-		long ret;
-		register long r10 __asm__("r10") = a4;
-		register long r8 __asm__("r8") = a5;
-		register long r9 __asm__("r9") = a6;
-		__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(num), "D"(a1), "S"(a2), "d"(a3), "r"(r10), "r"(r8), "r"(r9) : "memory", "rcx", "r11");
-		return ret;
-	}
+template <class T1, class T2, class T3, class T4, class T5>
+long syscall(long num, T1 a1, T2 a2, T3 a3, T4 a4, T5 a5) {
+	long ret;
+	register long n __asm__("rax")  = (num);
+	register long r1 __asm__("rdi") = (long)(a1);
+	register long r2 __asm__("rsi") = (long)(a2);
+	register long r3 __asm__("rdx") = (long)(a3);
+	register long r4 __asm__("r10") = (long)(a4);
+	register long r5 __asm__("r8")  = (long)(a5);
+
+	__asm__ volatile(
+		"syscall\n"
+		: "=a"(ret)
+		: "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5),
+		  "0"(n)
+		: "rcx", "r11", "memory", "cc");
+	return ret;
+}
+
+template <class T1, class T2, class T3, class T4, class T5, class T6>
+long syscall(long num, T1 a1, T2 a2, T3 a3, T4 a4, T5 a5, T6 a6) {
+	long ret;
+	register long n __asm__("rax")  = (num);
+	register long r1 __asm__("rdi") = (long)(a1);
+	register long r2 __asm__("rsi") = (long)(a2);
+	register long r3 __asm__("rdx") = (long)(a3);
+	register long r4 __asm__("r10") = (long)(a4);
+	register long r5 __asm__("r8")  = (long)(a5);
+	register long r6 __asm__("r9")  = (long)(a6);
+
+	__asm__ volatile(
+		"syscall\n"
+		: "=a"(ret)
+		: "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6), "0"(n)
+		: "rcx", "r11", "memory", "cc");
+	return ret;
+}
 #endif
 }
 
