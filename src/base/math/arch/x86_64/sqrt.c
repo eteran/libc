c5053d106c3249b2725dac59f17512535ba67af1
Evan Teran <evan.teran@gmail.com>
2026-03-24 15:56:45 -0400

    avoid intrinsics

--- src/base/math/arch/x86_64/sqrt.c
+++ src/base/math/arch/x86_64/sqrt.c
@@ -4,10 +4,6 @@
 #include <fenv.h>
 #include <math.h>
 
-#ifdef __SSE_MATH__
-#include <immintrin.h>
-#endif
-
 #ifdef _HAS_FPU
 
 /**
@@ -15,17 +11,14 @@
  *
  * @param x the floating point number to compute the square root of
  * @return the square root of the floating point number
- * @note This function does not handle special cases such as NaN or infinity
- *       gracefully.
  */
 _ALWAYS_INLINE _INLINE static double __elibc_sqrt(double x) {
 #ifdef __SSE2_MATH__
-	__m128d xmm0 = _mm_set_sd(x);
-	__m128d xmm1;
+	double value;
 	__asm__ __volatile__("sqrtsd %1, %0"
-						 : "=x"(xmm1)
-						 : "x"(xmm0));
-	return _mm_cvtsd_f64(xmm1);
+						 : "=x"(value)
+						 : "x"(x));
+	return value;
 #else
 	double value;
 	__asm__ __volatile__("fsqrt"
